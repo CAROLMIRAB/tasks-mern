a7d9b913c4a8c7c6135edb7e0dca474b7cd66c99
Carol Mirabal <carolmirabal27@gmail.com>
2018-06-19 12:02:04 -0300

    version inicial

---
+++ src/routes/task.routes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const Task = require('../models/task'); 
+
+router.get('/', async (req, res) => {
+   const tasks = await Task.find();
+   console.log(tasks);
+    res.json(tasks);
+});
+
+router.post('/', async (req, res)=>{
+    console.log(req.body);
+})
+
+module.exports = router;
